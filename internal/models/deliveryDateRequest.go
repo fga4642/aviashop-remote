02e129fbdc0e8ea9dc1b80d6b105260c0f6727ab
YarikMix <fga4642@yandex.ru>
2023-12-30 20:48:21 +0300

    Clean

--- internal/models/deliveryDateRequest.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type DeliveryDateRequest struct {
-	AccessToken int64  `json:"access_token"`
-	DeliveryDate int `json:"delivery_date"`
-}
--- internal/models/request.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type Request struct {
-	AccessToken int64 `json:"access_token"`
-	OrderId    int64 `json:"order_id"`
-}
